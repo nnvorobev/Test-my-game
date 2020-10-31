1ef0d367b8ab7819ad05d9526259a1f158a316f3
nnvorobev <nnvorobev@gmail.com>
2020-10-31 22:48:09 +0300

    Add difficulty

--- Test my game/GameViewController.swift
+++ Test my game/GameViewController.swift
@@ -11,7 +11,9 @@ class GameViewController: UIViewController {
 
 //    MARK: - Properties
         var ship: SCNNode!
-    
+        var score=0
+        var duration:TimeInterval=5
+
 //    MARK: - Metods
         func addShip() {
         //Move ship father from view
@@ -23,7 +25,11 @@ class GameViewController: UIViewController {
             
             //Animate ship movement towards camera
             //        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-            ship.runAction(.move(to: SCNVector3(), duration: 5))
+            ship.runAction(.move(to: SCNVector3(), duration: duration)) {
+                self.ship.removeFromParentNode()
+                
+                print(#line,#function, "Game over")
+            }
             
             //Make the ship look at given point
             ship.look(at:SCNVector3(2*x, 2*y, 2*z))
@@ -132,16 +138,19 @@ class GameViewController: UIViewController {
             
             // highlight it
             SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
+            SCNTransaction.animationDuration = 0.2
             
             // on completion - unhighlight
             SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
+                self.ship.removeFromParentNode()
+                self.score+=1
                 
-                material.emission.contents = UIColor.black
+                print(#line,#function, "The ship \(self.score) has been shot")
                 
-                SCNTransaction.commit()
+                self.duration*=0.95
+                self.ship=self.getShip()
+                self.addShip()
+               
             }
             
             material.emission.contents = UIColor.red
